7c953a01aff9835f2fb3074a11ab61230d9f34ff
Graeme Robertson <graeme.robertson@metaswitch.com>
2014-04-28 11:50:46 +0000

    Integrate support for multiple diameter connections with Ralf

--- include/peer_message_sender_factory.hpp
+++ include/peer_message_sender_factory.hpp
@@ -40,10 +40,16 @@
 
 class PeerMessageSenderFactory {
 public:
+
+  PeerMessageSenderFactory(const std::string& dest_realm) : _dest_realm(dest_realm) {};
+
   virtual PeerMessageSender* newSender(SAS::TrailId trail)
   {
-    return new PeerMessageSender(trail);
+    return new PeerMessageSender(trail, _dest_realm);
   }
+
+private:
+  const std::string _dest_realm;
 };
 
 
