d4dcaf3c77d1263dc1fd6784f628be87f82985a5
Eleanor Merry <eleanor.merry@metaswitch.com>
2017-11-08 18:29:12 +0000

    Actually check in changes

--- debian/ralf.init.d
+++ debian/ralf.init.d
@@ -105,10 +105,11 @@ get_daemon_args()
         [ "$sas_use_signaling_interface" != "Y" ] || sas_signaling_if_arg="--sas-use-signaling-interface"
 
         [ -z "$diameter_timeout_ms" ] || diameter_timeout_ms_arg="--diameter-timeout-ms=$diameter_timeout_ms"
-        [ -z "$target_latency_us" ] || target_latency_us_arg="--target-latency-us=$target_latency_us"
-        [ -z "$max_tokens" ] || max_tokens_arg="--max-tokens=$max_tokens"
-        [ -z "$init_token_rate" ] || init_token_rate_arg="--init-token-rate=$init_token_rate"
-        [ -z "$min_token_rate" ] || min_token_rate_arg="--min-token-rate=$min_token_rate"
+        [ -z "$ralf_target_latency_us" ] || target_latency_us_arg="--target-latency-us=$ralf_target_latency_us"
+        [ -z "$ralf_max_tokens" ] || max_tokens_arg="--max-tokens=$ralf_max_tokens"
+        [ -z "$ralf_init_token_rate" ] || init_token_rate_arg="--init-token-rate=$ralf_init_token_rate"
+        [ -z "$ralf_min_token_rate" ] || min_token_rate_arg="--min-token-rate=$ralf_min_token_rate"
+        [ -z "$ralf_max_token_rate" ] || max_token_rate_arg="--max-token-rate=$ralf_max_token_rate"
         [ -z "$exception_max_ttl" ] || exception_max_ttl_arg="--exception-max-ttl=$exception_max_ttl"
         [ -z "$cdf_identity" ] || billing_peer_arg="--billing-peer=$cdf_identity"
         [ -z $signaling_namespace ] || namespace_prefix="ip netns exec $signaling_namespace"
@@ -138,6 +139,7 @@ get_daemon_args()
                      $max_tokens_arg
                      $init_token_rate_arg
                      $min_token_rate_arg
+                     $max_token_rate_arg
                      $exception_max_ttl_arg
                      $sas_signaling_if_arg
                      --sas=$sas_server,$NAME@$public_hostname"
