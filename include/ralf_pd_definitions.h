693c844b64306153669c14dbf388e281c200a0e5
Adam Krajovsky <adam.krajovsky@metaswitch.com>
2018-03-08 15:57:45 +0000

    log improvements

--- include/ralf_pd_definitions.h
+++ include/ralf_pd_definitions.h
@@ -129,13 +129,12 @@ static const PDLog1<const char*> CL_RALF_CDF_CONNECTION_ERROR
   PDLogBase::CL_RALF_ID + 10,
   LOG_INFO,
   "Connections between Ralf and CDF applications at %s have failed.",
-  "At least one of the Diameter peers is not contactable and the number of contactable "
+  "At least one of the CDF instances is not contactable and the number of contactable "
   "instances is strictly less than expected (two unless changed through configuration)",
-  "This process was unable to contact some instances of the application "
-  "it's trying to connect to",
-  "(1). Check that the application this process is trying to connect to is running."
-  "(2). Check the configuration in /etc/clearwater is correct."
-  "(3). Check that this process has connectivity to the application it's trying to connect to."
+  "This process was unable to contact some of the CDF instances it's trying to connect to",
+  "(1). Check that the listed CDF applications are running."
+  "(2). Check the CDF configuration in /etc/clearwater is correct."
+  "(3). Check that this process has connectivity to the listed CDF instances."
 );
 
 static const PDLog CL_RALF_CDF_CONNECTION_CLEARED
@@ -143,7 +142,7 @@ static const PDLog CL_RALF_CDF_CONNECTION_CLEARED
   PDLogBase::CL_RALF_ID + 11,
   LOG_INFO,
   "Some connections between Ralf and CDF have been restored.",
-  "Either all known Diameter peers are conntactable, or there are enough contactable peers (two "
+  "Either all known CDF instances are contactable, or there are enough contactable instances (two "
   "unless changed through configuration) to provide the required level of Diameter peer resilience.",
   "Normal.",
   "None."
